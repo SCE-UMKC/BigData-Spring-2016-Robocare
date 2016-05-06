d8b9055d11c6bc757d5398531dc0747478b2dbed
krishna578 <vp327@mail.umkc.edu>
2016-05-06 17:20:40 -0600

    android features

--- Android  Features/app/src/main/java/com/example/daras/robocare/Register.java
+++ Android  Features/app/src/main/java/com/example/daras/robocare/Register.java
@@ -18,8 +18,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import com.example.daras.robocare.UserDetails;
-
 public class Register extends AppCompatActivity {
 
 
