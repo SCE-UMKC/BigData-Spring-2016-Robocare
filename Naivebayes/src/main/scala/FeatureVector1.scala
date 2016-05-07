7375870fe58cf0d591dd615c4f122916d845042c
Sandeep <dara.sandeep91@gmail.com>
2016-05-07 17:05:31 -0500

    Naive Bayes

---
+++ Naivebayes/src/main/scala/FeatureVector1.scala
@@ -0,0 +1,41 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.mllib.classification.{NaiveBayes, NaiveBayesModel}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+
+object FeatureVector1 {
+
+  def main(args: Array[String]) {
+    System.setProperty("hadoop.home.dir", "F:\\winutils")
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Spark-Machine_Learning-Text-1").set("spark.driver.memory", "3g").set("spark.executor.memory", "3g")
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
+    val sc = ssc.sparkContext
+
+    val data = sc.textFile("/Dataset.csv")
+    val parsedData = data.map { line =>
+      val parts = line.split(',')
+      LabeledPoint(parts(0).toDouble, Vectors.dense(parts(1).split(' ').map(_.toDouble)))
+    }
+
+     //Split data into training (60%) and test (40%).
+    val splits = parsedData.randomSplit(Array(0.6, 0.4), seed = 11L)
+    val training = splits(0)
+    val test = splits(1)
+
+    val model = NaiveBayes.train(parsedData, lambda = 1.0, modelType = "multinomial")
+
+    val predictionAndLabel = test.map(p => (model.predict(p.features), p.label))
+    val accuracy = 1.0 * predictionAndLabel.filter(x => x._1 == x._2).count() / test.count()
+
+    predictionAndLabel.foreachPartition(Row=>
+    {
+      println(Row)
+    })
+    // Save and load model
+    model.save(sc, "target/tmp1/myNaiveBayesModel")
+    val sameModel = NaiveBayesModel.load(sc, "target/tmp1/myNaiveBayesModel")
+
+  }
+}
